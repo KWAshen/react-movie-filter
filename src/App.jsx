ac0cfa6100856d43bfd4e2372f74b15397e1cf64
KWAshen <ashen.maliaha@gmail.com>
2025-09-07 23:57:24 +0200

    fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 const typemovies = [
    { title: 'Inception', genre: 'Fantascienza' },
@@ -16,7 +16,8 @@ function App() {
 
   useEffect(() =>{
     if(genre !== ''){
-      const selectdmovies = movies.filter(movie => movie.genre ===genre);
+      const selectdmovies = movies.filter
+      (movie => movie.genre ===genre);
       setmovies(selectdmovies);
     }
     else
@@ -30,25 +31,33 @@ function App() {
     <div className="container my-5">
       <div className="row gy-4">
         <div className="col-12">
-          <select name="" id="" className="form select my-3">
-            <option value="">Select Type</option>
-            {typemovies.map((movie,index) => {
-              return(
-                <option value={movie.genre}key={`genre-${index}`}>{movie.genre}</option>
-              )
-            })}              
-          </select>
-    <h1>react-movie-filter</h1>
-        <div className="col-12">
-          <ul className="list-group ms-0">
-            {typemovies.map((movie, index) =>{
+          <h1>react-movies-form</h1>
+          </div>
+          <div className="col-12">
+            <select name=""
+            id=""
+            className="form-select my-3"
+            value={genre}
+            onChange={(e)=> setgenre(e.target.value)}
+            >
+              <option value="">genere</option>
+              <option value="Fantascienza">Fantascienza</option>
+              <option value="Thriller">Thriller</option>
+              <option value="Azione">Azione</option>
+              <option value="Romantico">Romantico</option>
+              </select> 
+
+            <ul className="list-group ms-0">
+              {movies.map((movie,index)=>
+              {typemovies.map((movie, index) =>{
               return (
-                <li key={index} className="list-group-item">{movie.title}
+                <li key={index} className="list-group-item">
+                  {movie.title}
                 </li>
-              )
+              );
             })}
-          </ul>
-        </div>
+            </ul>
+            </div>
         </div>
       </div>
     </div>
