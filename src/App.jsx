a33d4a0a8b7efb552b06da2aff233e24b73c31ad
KWAshen <ashen.maliaha@gmail.com>
2025-09-07 21:06:47 +0200

    primo salvataggio

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+const typemovies = [
+   { title: 'Inception', genre: 'Fantascienza' },
+   { title: 'Il Padrino', genre: 'Thriller' },
+   { title: 'Titanic', genre: 'Romantico' },
+   { title: 'Batman', genre: 'Azione' },
+   { title: 'Interstellar', genre: 'Fantascienza' },
+   { title: 'Pulp Fiction', genre: 'Thriller' },
+ ]
+
+function App() {
+
+
+  return (
+    <>
+    <h1>react-movie-filter</h1>
+    </>
+  )
+}
+
+export default App
