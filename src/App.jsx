de329e562817647390cf8bc46026b78736808aa9
KWAshen <ashen.maliaha@gmail.com>
2025-09-07 21:49:20 +0200

    primo pezzo di codice

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,31 @@ function App() {
 
   return (
     <>
+    <div className="container my-5">
+      <div className="row gy-4">
+        <div className="col-12">
+          <select name="" id="" className="form select">
+            <option value="">Select Type</option>
+            {typemovies.map((movie,index) => {
+              return(
+                <option value={movie.genre}key={`genre-${index}`}>{movie.genre}</option>
+              )
+            })}              
+          </select>
     <h1>react-movie-filter</h1>
+        <div className="col-12">
+          <ul className="list-group ms-0">
+            {typemovies.map((movie, index) =>{
+              return (
+                <li key={index} className="list-group-item">{movie.title}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
